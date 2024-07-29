99e0618fc020e3431e5067853334baea5bfe44fa
Vermaas <vermaas@astron.nl>
2024-07-29 14:37:03 +0200

    update collect_h5.py

--- scripts/collect_h5.py
+++ scripts/collect_h5.py
@@ -1,14 +1,17 @@
 import sys
 sys.argv
 
+inputs = sys.argv[1]
+
 # read input file
-with open('inputs.txt', 'r', encoding='utf-8') as input_file:
+with open(inputs, 'r', encoding='utf-8') as input_file:
     lines = input_file.readlines()
     for line in lines:
         print(line)
 
+outputs = inputs.replace('inputs.txt','outputs.txt')
 # write output file
-with open('outputs.txt', 'w', encoding='utf-8') as output_file:
+with open(outputs, 'w', encoding='utf-8') as output_file:
     output_file.write('my_storage_location\658584\658583_aggregated.h5')
 
 print('succesfully executed collect_h5.py')
