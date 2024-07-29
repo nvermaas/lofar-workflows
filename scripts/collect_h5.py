b32c27c80a086096a14f1e6c23283caacec4d12d
Vermaas <vermaas@astron.nl>
2024-07-29 14:27:34 +0200

    update collect_h5.py

--- scripts/collect_h5.py
+++ scripts/collect_h5.py
@@ -1,1 +1,14 @@
-print('my_storage_location\658584\658583_aggregated.h5')
+import sys
+sys.argv
+
+# read input file
+with open('inputs.txt', 'r', encoding='utf-8') as input_file:
+    lines = input_file.readlines()
+    for line in lines:
+        print(line)
+
+# write output file
+with open('outputs.txt', 'w', encoding='utf-8') as output_file:
+    output_file.write('my_storage_location\658584\658583_aggregated.h5')
+
+print('succesfully executed collect_h5.py')
