50ea47a53ca856116a1e4fa4a07a7baba7220b8f
Vermaas <vermaas@astron.nl>
2024-08-02 09:00:03 +0200

    fix for use on spider

--- scripts/collect_h5.py
+++ scripts/collect_h5.py
@@ -12,6 +12,8 @@ with open(inputs, 'r', encoding='utf-8') as input_file:
 outputs = inputs.replace('inputs.txt','outputs.txt')
 
 file_path = os.path.abspath('658583_aggregated.h5')
+the_dir = os.path.split(line)[0]
+file_path = os.path.join(the_dir,'658583_aggregated.h5')
 
 # write outputs.txt file
 with open(outputs, 'w', encoding='utf-8') as output_file:
