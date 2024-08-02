60a6eeb2d1bdb17c184a1f453bb443608869eaf1
Vermaas <vermaas@astron.nl>
2024-08-02 08:15:59 +0200

    fix for use on spider

--- scripts/collect_h5.py
+++ scripts/collect_h5.py
@@ -11,12 +11,14 @@ with open(inputs, 'r', encoding='utf-8') as input_file:
 
 outputs = inputs.replace('inputs.txt','outputs.txt')
 
+file_path = os.path.abspath('658583_aggregated.h5')
+
 # write outputs.txt file
 with open(outputs, 'w', encoding='utf-8') as output_file:
-    output_file.write('my_storage_location\\658584\\658583_aggregated.h5')
+    output_file.write(file_path)
 
 # write the aggregated file (simulated)
-with open('my_storage_location\\658584\\658583_aggregated.h5', 'w', encoding='utf-8') as output_file:
+with open(file_path, 'w', encoding='utf-8') as output_file:
     output_file.write('this is a simulated aggregated H5 file')
 
 
